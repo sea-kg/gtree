f26fc20b1b447362ba1c0a5ba562b8e313b55fde
Evgenii Sopov <mrseakg@gmail.com>
2019-07-29 02:53:04 +0700

    Fixed links

--- www/index.php
+++ www/index.php
@@ -24,7 +24,7 @@
           <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav mr-auto">
               <li class="nav-item active">
-                <a class="nav-link" href="persons.php">Дерево</a>
+                <a class="nav-link" href="index.php">Дерево</a>
               </li>
               <li class="nav-item active">
                 <a class="nav-link" href="persons.php">Персоны</a>
