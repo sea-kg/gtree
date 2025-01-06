95d166d67fd154844d98ac073dbada034fc2ce2a
Evgenii Sopov <mrseakg@gmail.com>
2025-01-06 17:32:48 +0700

    Updated contact.php

--- www/contact.php
+++ www/contact.php
@@ -1,11 +1,13 @@
 <?php
-      include_once("gtree.php"); 
+      include_once("gtree.php");
       include_once("head.php");
 ?>
       <div class="container">
         <h1>Обратная связь</h1>
-        Все вопросы и предложения слать на электронный адрес: 
-        <a href="mailto:mrseakg@gmail.com">mrseakg@gmail.com</a>
+        Все вопросы и предложения слать на электронный адрес:<br>
+        <a href="mailto:sea-kg@ya.ru">sea-kg@ya.ru</a><br>
+        Или в телеграм:<br>
+        <a href="https://t.me/sea5kg">https://t.me/sea5kg</a><br>
       </div>
   </body>
 </html>
