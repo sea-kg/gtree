026d88781fb97fe1f4942d42d3f3e052390b3c01
Evgenii Sopov <mrseakg@gmail.com>
2025-01-07 16:08:00 +0700

    Prepare

---
+++ src/main.cpp
@@ -0,0 +1,48 @@
+// #include <argument_processor_ctf01d_main.h>
+// #include <wsjcpp_core.h>
+// #include <employ_config.h>
+
+int main(int argc, const char* argv[]) {
+    // std::string TAG = "MAIN";
+    // std::string appName = std::string(WSJCPP_APP_NAME);
+    // std::string appVersion = std::string(WSJCPP_APP_VERSION);
+
+    // // previous logs in current directory
+    // if (!WsjcppCore::dirExists(".ctf01d")) {
+    //     WsjcppCore::makeDir(".ctf01d");
+    // }
+    // WsjcppLog::setPrefixLogFile("ctf01d");
+    // WsjcppLog::setLogDirectory(".ctf01d");
+
+    // // try find config.yml
+    // std::vector<std::string> vPossibleFolders = {
+    //     "./",
+    //     "./data_sample/",
+    //     "/root/data/"
+    // };
+
+    // for (int i = 0; i < vPossibleFolders.size(); i++) {
+    //     std::string sWorkDir = vPossibleFolders[i];
+    //     if (sWorkDir[0] != '/') {
+    //         sWorkDir = WsjcppCore::getCurrentDirectory() + "/" + sWorkDir;
+    //     }
+    //     sWorkDir = WsjcppCore::doNormalizePath(sWorkDir);
+    //     if (WsjcppCore::fileExists(sWorkDir + "/config.yml")) {
+    //         std::cout << "Automatically detected workdir: " << sWorkDir << std::endl;
+    //         EmployConfig *pConfig = findWsjcppEmploy<EmployConfig>();
+    //         pConfig->setWorkDir(sWorkDir);
+    //         break;
+    //     }
+    // }
+
+    // // websocket_server_t server;
+    // // server.service = pRouter;
+    // // server.port = 12345;
+    // // // server.ws = pWs;
+    // // websocket_server_run(&server);
+
+    // ArgumentProcessorCtf01dMain *pMain = new ArgumentProcessorCtf01dMain();
+    // WsjcppArguments prog(argc, argv, pMain);
+    // int nRet = prog.exec();
+    // return nRet;
+}
